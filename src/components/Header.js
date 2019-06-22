95b95d1dcc3d3ca242462679cb63949d255f2ced
kchy1234 <kchy12345@gmail.com>
2019-06-22 18:07:16 +0900

    stopwatch 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Stats} from './Stats';
+import React from "react";
+import {Stats} from "./Stats";
+import {Stopwatch} from "./Stopwatch";
 
 export const Header = ({title, totalPlayers, players}) => {
   // console.log(props);
@@ -8,6 +9,7 @@ export const Header = ({title, totalPlayers, players}) => {
     <header className="header">
       <Stats players={players}/>
       <h1 className="h1">{title}</h1>
+      <Stopwatch />
     </header>
   )
 }
