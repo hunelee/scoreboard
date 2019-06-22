7c29ae7607d51e64e3e47850d78ac2f72b1bb0f4
kchy1234 <kchy12345@gmail.com>
2019-06-22 12:15:06 +0900

    migration from cdn

--- src/App.js
+++ src/App.js
@@ -2,25 +2,101 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Header = ({title, totalPlayers}) => {
+ // console.log(props);
+ // const {title, totalPlayers} = props; // destruct assignment
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header className="header">
+      <h1 className="h1">{title}</h1>
+      <span className="stats">Players: {totalPlayers}</span>
+    </header>
+  )
+}
+
+const Player = (props) => (
+  <div className="player">
+    <span className="player-name">
+      <button className="remove-player" onClick={() => props.removePlayer(props.id)}>X</button>
+      {props.name}
+    </span>
+    <Counter />
+  </div>
+)
+
+// function component
+// const Counter = (props) => (
+//   <div className="counter">
+//     <button className="counter-action decrement"> - </button>
+//     <span className="counter-score">{props.score}</span>
+//     <button className="counter-action increment"> + </button>
+//   </div>
+// )
+
+// class component
+class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      score: 0,
+      time: 10
+    };
+    // this.incrementScore = this.incrementScore.bind(this);
+  }
+  
+  // 애로우펑션안의 this는 lexical this로써 자기자신을 가리키게 된다
+  handleScore = (delta) => {
+    console.log('increment', this);
+    
+    // this.state.score +=1;
+    // setState를 호출해야만 UI 렌더링이 된다
+    // this.setState({score: this.state.score + 1});
+    this.setState(prevState => {
+      return {score: prevState.score + delta}
+    })
+  }
+  
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={() => this.handleScore(-1)}> - </button>
+        <span className="counter-score">{this.state.score}</span>
+        <button className="counter-action increment" onClick={() => this.handleScore(1)}> + </button>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    players: [
+      {name: 'AAA', id: 1},
+      {name: 'BBB', id: 2},
+      {name: 'CCC', id: 3},
+      {name: 'DDD', id: 4}
+    ]
+  }
+  
+  handleRemovePlayer = (id) => {
+    console.log('remove player: ', id);
+    
+    this.setState(prevState => ({
+      players: prevState.players.filter(item => item.id !== id)
+    }))
+  }
+  
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header title="My Scoreboard" totalPlayers={11} />
+        
+        {
+          this.state.players.map(player => (
+            <Player name={player.name} id={player.id} key={player.id} removePlayer={this.handleRemovePlayer} />
+          ))
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
