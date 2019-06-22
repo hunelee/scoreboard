8dee3d3229d04b3eeba117769be51ba56d180cf6
kchy1234 <kchy12345@gmail.com>
2019-06-22 12:55:59 +0900

    컴포넌트를 모듈 분리 완료

--- src/App.js
+++ src/App.js
@@ -1,50 +1,7 @@
 import React from 'react';
 import {Header} from './components/Header';
 import './App.css';
-
-const Player = (props) => (
-  <div className="player">
-    <span className="player-name">
-      <button className="remove-player" onClick={() => props.removePlayer(props.id)}>X</button>
-      {props.name}
-    </span>
-    <Counter />
-  </div>
-)
-
-// class component
-class Counter extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      score: 0,
-      time: 10
-    };
-    // this.incrementScore = this.incrementScore.bind(this);
-  }
-  
-  // 애로우펑션안의 this는 lexical this로써 자기자신을 가리키게 된다
-  handleScore = (delta) => {
-    console.log('increment', this);
-    
-    // this.state.score +=1;
-    // setState를 호출해야만 UI 렌더링이 된다
-    // this.setState({score: this.state.score + 1});
-    this.setState(prevState => {
-      return {score: prevState.score + delta}
-    })
-  }
-  
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={() => this.handleScore(-1)}> - </button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={() => this.handleScore(1)}> + </button>
-      </div>
-    )
-  }
-}
+import {Player} from "./components/Player";
 
 class App extends React.Component {
   state = {
