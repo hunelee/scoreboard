cf6b649eff93841dd6ec19f3b94e632ea61370e6
kchy1234 <kchy12345@gmail.com>
2019-06-29 10:21:55 +0900

    Player 컴포넌트에 객체 해체 할당 적용

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,14 +4,15 @@ import {Counter} from "./Counter";
 export class Player extends React.Component {
   render() {
     console.log(this.props.name, ' rendered');
+    const {removePlayer, id, name, score, changeScore} = this.props;
     
     return (
       <div className="player">
         <span className="player-name">
-          <button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}>X</button>
-          {this.props.name}
+          <button className="remove-player" onClick={() => removePlayer(id)}>X</button>
+          {name}
         </span>
-        <Counter score={this.props.score} id={this.props.id} changeScore={this.props.changeScore}/>
+        <Counter score={score} id={id} changeScore={changeScore}/>
       </div>
     )
   }
