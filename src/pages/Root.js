cfb5f0fc6df892a8eb238813fa6c2b20ae43a8e1
kchy1234 <kchy12345@gmail.com>
2019-06-29 18:22:56 +0900

    메뉴바 만들기 - heroes 메뉴 생성

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -3,13 +3,14 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Home} from "./Home";
 import {Heroes} from "./Heroes";
 import {Scoreboard} from "./Scoreboard";
+import {Menu} from "./Menu";
 
 export class Root extends React.Component {
   
   render() {
     return (
       <BrowserRouter>
-        <p>공통메뉴</p>
+        <Menu></Menu>
         {/*네스티드 라우팅 영역*/}
         <Switch>
           <Route exact path="/" component={Home}></Route>
