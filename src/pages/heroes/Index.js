8dbad66aeca7126045cd60925ee9c4bf46427187
kchy1234 <kchy12345@gmail.com>
2019-07-06 17:03:32 +0900

    heroes 메뉴로 이동시 첫번째 메뉴로 리다이렉트

---
+++ src/pages/heroes/Index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Nav from "reactstrap/es/Nav";
+import NavItem from "reactstrap/es/NavItem";
+import {NavLink, Redirect, Route, Switch} from "react-router-dom";
+import {Heroes} from "./Heroes";
+import {Register} from "./Register";
+
+import './Index.scss';
+
+export class Index extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <Nav>
+          <NavItem>
+            <NavLink to="/heroes/hero" className="nav-link">Hero List</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/heroes/register" className="nav-link">Register</NavLink>
+          </NavItem>
+        </Nav>
+        
+        <Switch>
+          <Route path="/heroes/hero" component={Heroes}></Route>
+          <Route path="/heroes/register" component={Register}></Route>
+          <Route path="/heroes" render={() => <Redirect to ="/heroes/hero" />}></Route>
+        </Switch>
+      </div>
+    );
+  }
+}
